7eac6602fd99cc99f277db89986b27023f17252f
Mark Warunge <mmwarunge@gmail.com>
2026-04-24 21:41:00 +0300

    App edit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, session
-from data import articles   # assumes articles list/dict exists in data.py
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
