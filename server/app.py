e4b79c00060de4a5f9291f6b3e9a13d2bfcdb69f
Mark Warunge <mmwarunge@gmail.com>
2026-04-24 21:44:50 +0300

    App final edit

--- server/app.py
+++ server/app.py
@@ -1,32 +1,46 @@
 from flask import Flask, jsonify, session
 from models import db, Article
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
 
-@app.route('/articles/<int:id>')
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+@app.route("/articles/<int:id>")
 def show_article(id):
-    # initialize session counter
+
     if "page_views" not in session:
         session["page_views"] = 0
 
-    # block after 3 views
     if session["page_views"] >= 3:
         return jsonify({
             "message": "Maximum pageview limit reached"
         }), 401
 
-    # find article by id
-    article = next((article for article in articles if article["id"] == id), None)
+    article = Article.query.filter_by(id=id).first()
 
     if not article:
         return jsonify({"message": "Article not found"}), 404
 
-    # increment views after successful view
     session["page_views"] += 1
 
-    return jsonify(article), 200
+    return jsonify({
+        "id": article.id,
+        "title": article.title,
+        "author": article.author,
+        "content": article.content,
+        "preview": article.preview,
+        "minutes_to_read": article.minutes_to_read,
+        "date": article.date
+    }), 200
 
 
-if __name__ == '__main__':
-    app.run(port=5555)
+if __name__ == "__main__":
+    app.run(debug=True)
